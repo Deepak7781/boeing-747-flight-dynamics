e9eeb95426633c69a453bd9351657603e8305a10
Deepak7781 <deepak21082005@gmail.com>
2026-06-05 12:37:04 +0530

    complete: main.py

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,15 @@
+import control
+import numpy as np
 from boeing747Params import Params
 from stateSpaceModel import stateSpace
 p = Params()
 
 lonSS, latSS = stateSpace(p)
-print(lonSS)
 
-print(latSS)
+Alon = lonSS.A
+lonEigen = np.linalg.eig(Alon)
+print(lonEigen.eigenvalues)
+
+Alat = latSS.A
+latEigen = np.linalg.eig(Alat)
+print(latEigen.eigenvalues)
